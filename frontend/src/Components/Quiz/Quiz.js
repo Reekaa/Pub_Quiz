bceaf8dd2812fe91f614981ffa3b95a042a663fb
Euan Gilmour <a9404253@9404253s-MacBook-Pro.local>
2019-07-23 11:51:46 +0100

    Set up basic Quiz files

---
+++ frontend/src/Components/Quiz/Quiz.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import PlaceHolder from './Placeholder/Placeholder'
+import Question from './Question/Question'
+import Answer from './Answer/Answer'
+import Result from './Result/Result'
+
+class Quiz extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showModal: 'placeholder'
+    }
+  }
+
+  setModal = (value) => {
+    this.setState({showModal: value})
+  }
+
+  render() {
+    return (
+      <div className="quiz-container">
+        {this.state.showModal === 'placeholder' && <PlaceHolder setModal={this.setModal}/>}
+        {this.state.showModal === 'question' && <Question />}
+        {this.state.showModal === 'answer' && <Answer />}
+        {this.state.showModal === 'result' && <Result />}
+      </div>
+    );
+  }
+}
+
+export default Quiz
