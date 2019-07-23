bb8dee13d28d2813b32213e7ddf3ef03a7fe712f
bsharp807 <a0030619@0030619s-MacBook-Pro.local>
2019-07-23 11:28:28 +0100

    index and team reducers created

---
+++ frontend/src/_reducers/team_reducer.js
@@ -0,0 +1,13 @@
+const teamReducer = (state=[], action) => {
+  switch (action.type) {
+    case 'ADD_TEAM_DATA':
+      const newState = [...state, action.team]
+      return newState;
+    case 'BLITZ_TEAM_DATA':
+      return []
+    default:
+    return state;
+  };
+};
+
+export default teamReducer;
