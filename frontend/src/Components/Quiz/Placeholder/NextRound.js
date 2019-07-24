f3306e45edcdc624efb44a0e86cc34117e1b26c9
Rose Ulldemolins <a9854261@9854261s-MacBook-Pro.local>
2019-07-24 10:25:19 +0100

    added button from library and hardcoded placeholder text

---
+++ frontend/src/Components/Quiz/Placeholder/NextRound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NextRound = (props) => {
+
+  return(
+    <div>
+      <div className='next-round-text' >Your next round is bla bla bla</div>
+    </div>
+  )
+}
+
+export default NextRound
--- frontend/src/Components/Quiz/Placeholder/Placeholder.js
+++ frontend/src/Components/Quiz/Placeholder/Placeholder.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
+import NextRound from './NextRound';
+import Button from '../../../PatternLibrary/Button'
 
 const Placeholder = (props) => {
 
-  const handleGo = () => {
-    props.setModal('question')
-  }
-
   return(
     <div>
-      <button className='button' onClick={() => {handleGo()}}>Go!</button>
+      <NextRound />
+      <Button
+      function={() => {props.setModal('question')}} buttonText='Go!'/>
     </div>
   )
-}
+};
 
 export default Placeholder
